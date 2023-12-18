d8639a83c4ee8fc71237b434209644fdc9449730
Karthick Palanivel <103199567+karthickpalanivel@users.noreply.github.com>
2023-12-18 14:14:20 +0530

    added leetcode problems module

---
+++ leetCode-problem/subStringOccurence.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class subStringOccurence {
+    public static void main(String[] args) {
+        int x = 5;
+System.out.println(true && false);
+System.out.println(++x);
+        
+    }
+
+    public static boolean solution(String str) {
+        int n = str.length();
+        str = str.toLowerCase();
+        
+        if (n == 0 || n == 1) {
+            return false;
+        }
+
+        HashSet<Character> set = new HashSet<Character>();
+        char[] ch = str.toCharArray();
+        
+
+        for (int i = 0; i < n; i++) {
+            set.add(ch[i]);
+        }
+
+        char[] ch1 = new char[set.size()];
+
+        for (char i : set) {
+            ch1[i] = i;
+        }
+
+        String subString = new String(ch1);
+
+        System.out.print(subString);
+        if (str.contains(subString + subString)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
